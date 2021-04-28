8af2fca40ab92ae8440bec32d811b8956e719eb7
mvsrgc <private4f34rf34f34f34f34fasdasda32d23d32d@email.com>
2021-04-28 17:00:26 -0400

    IMGUI Wrapper.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,108 @@
-use ggez::{
-    conf,
-    event::{self, EventHandler},
-    Context, GameResult,
-};
+mod imgui_wrapper;
+
+use crate::imgui_wrapper::ImGuiWrapper;
+use ggez::event::{self, EventHandler, KeyCode, KeyMods, MouseButton};
+use ggez::nalgebra::Point2;
+use ggez::{conf, graphics, Context, GameResult};
+
 use orst::algos::bubblesort::BubbleSort;
 
 const APP_WIDTH: f32 = 1280.0;
 const APP_HEIGHT: f32 = 720.0;
 
-struct AppState {}
+struct MainState {
+    pos_x: f32,
+    imgui_wrapper: ImGuiWrapper,
+    hidpi_factor: f32,
+}
 
-impl AppState {
-    pub fn new(ctx: &mut Context) -> GameResult<AppState> {
-        let mut things = vec![3, 2, 4, 1, 6];
-
-        let bub = orst::sort::<_, BubbleSort>(&mut things);
-
-        println!("{:?}", things);
-
-        let s = AppState {};
-
+impl MainState {
+    fn new(mut ctx: &mut Context, hidpi_factor: f32) -> GameResult<MainState> {
+        let imgui_wrapper = ImGuiWrapper::new(&mut ctx);
+        let s = MainState {
+            pos_x: 0.0,
+            imgui_wrapper,
+            hidpi_factor,
+        };
         Ok(s)
     }
 }
 
-impl EventHandler for AppState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        todo!()
+impl EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
     }
 
-    fn draw(&mut self, _ctx: &mut Context) -> GameResult {
-        todo!()
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::BLACK);
+
+        // Render game stuff
+        {
+            let circle = graphics::Mesh::new_circle(
+                ctx,
+                graphics::DrawMode::fill(),
+                Point2::new(self.pos_x, 380.0),
+                100.0,
+                2.0,
+                graphics::WHITE,
+            )?;
+            graphics::draw(ctx, &circle, (Point2::new(0.0, 0.0),))?;
+        }
+
+        // Render game ui
+        {
+            self.imgui_wrapper.render(ctx, self.hidpi_factor);
+        }
+
+        graphics::present(ctx)?;
+        Ok(())
+    }
+
+    fn mouse_motion_event(&mut self, _ctx: &mut Context, x: f32, y: f32, _dx: f32, _dy: f32) {
+        self.imgui_wrapper.update_mouse_pos(x, y);
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        button: MouseButton,
+        _x: f32,
+        _y: f32,
+    ) {
+        self.imgui_wrapper.update_mouse_down(button);
+    }
+
+    fn mouse_button_up_event(&mut self, _ctx: &mut Context, button: MouseButton, _x: f32, _y: f32) {
+        self.imgui_wrapper.update_mouse_up(button);
+    }
+
+    fn key_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        keycode: KeyCode,
+        keymods: KeyMods,
+        _repeat: bool,
+    ) {
+        self.imgui_wrapper.update_key_down(keycode, keymods);
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, keymods: KeyMods) {
+        self.imgui_wrapper.update_key_up(keycode, keymods);
+    }
+
+    fn text_input_event(&mut self, _ctx: &mut Context, val: char) {
+        self.imgui_wrapper.update_text(val);
+    }
+
+    fn resize_event(&mut self, ctx: &mut Context, width: f32, height: f32) {
+        graphics::set_screen_coordinates(ctx, graphics::Rect::new(0.0, 0.0, width, height))
+            .unwrap();
+        //println!("{:?}", graphics::screen_coordinates(ctx));
+    }
+
+    fn mouse_wheel_event(&mut self, _ctx: &mut Context, x: f32, y: f32) {
+        self.imgui_wrapper.update_scroll(x, y);
     }
 }
 
@@ -41,7 +113,9 @@ fn main() -> GameResult {
 
     let (mut ctx, mut events_loop) = cb.build()?;
 
-    let mut app = AppState::new(&mut ctx)?;
+    let hidpi_factor = events_loop.get_primary_monitor().get_hidpi_factor() as f32;
+
+    let mut app = MainState::new(&mut ctx, hidpi_factor)?;
 
     event::run(&mut ctx, &mut events_loop, &mut app)
 }
